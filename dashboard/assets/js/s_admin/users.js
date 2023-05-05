49dcb69742e44ca971abb87868b3fa4ea8a36f7d
dannyroot <megamanxxx678@gmail.com>
2023-05-05 07:42:53 -0500

    modified:   dashboard/assets/js/s_admin/users.js 	modified:   dashboard/assets/modules/users.html

--- dashboard/assets/js/s_admin/users.js
+++ dashboard/assets/js/s_admin/users.js
@@ -1,19 +1,19 @@
-var dni = document.getElementById('dni')
 let selector = document.getElementById("inputGroupSelectType"); 
 createDatatable()
 
-dni.addEventListener('input', updateValue);
-function updateValue(e) {
-    e.target.value = e.target.value.replace(/[^0-9]/g, '').toLowerCase()
-    var xDNI = e.srcElement.value
-    if(xDNI.length == 8){
-      fetchDNI(xDNI)
-    }else{
-        clearInputs()
-    }
+let cacheV = localStorage.getItem("users")
+let vCache = JSON.parse(cacheV)
+
+
+if(vCache == null){
+  document.getElementById("bgspinner").style = "display:block;"
+  getUsersFromDatabase()
+}else{
+  document.getElementById("bgspinner").style = "display:none;"
+  getUsersFromCache()
 }
 
-      selector.addEventListener("change", () => {
+selector.addEventListener("change", () => {
       // if value switched by client
         if(selector.value == "1"){
             document.getElementById("li-b").style = "display:none;"
@@ -70,34 +70,168 @@ function showAddModal(){
   }
   
   function clearInputs(){
-    document.getElementById("dni").disabled = false
+    document.getElementById("dni").value = ""
     document.getElementById("fullname").value = ""
     document.getElementById("inputGroupSelectType").value = "0"
     document.getElementById("li").value = ""
-    document.getElementById("pass").value = ""
+    document.getElementById("password").value = ""
+    document.getElementById("phone").value = ""
+    document.getElementById("lastname").value = ""
 }
 
-function fetchDNI(value){
 
-let token = "63TM2UW53E8161HMCB9YRBVBDHIMZ5UT"
- 
-fetch('https://cowmanager.site/api/controllerDNI.php'+"?dni="+value,{
-  method: "GET",
-  headers: {
-      Authorization: `Bearer ${token}`
-  }
- })
-  .then(response => response.json())
-  .then(data => {
+function createUser(){
 
-      console.log(data)
+  let dni = document.getElementById("dni").value
+  let lastname = document.getElementById("lastname").value
+  let name = document.getElementById("fullname").value
+  let type = document.getElementById("inputGroupSelectType").value
+  let lic = document.getElementById("li").value
+  let phone = document.getElementById("phone").value
+  let password = document.getElementById("pass").value
+  let email = document.getElementById("email").value
 
-  }).catch(err => {
+  if(dni != "" && lastname !="" && name != "" && type != "" && password != "" && email != ""){
+  
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user.uid;
+
+    let data = {
+      dni : dni,
+      brandCar : null,
+      colorCar : null,
+      email : email,
+      id : user,
+      image : null,
+      lastname : lastname,
+      name : name,
+      phone : phone,
+      plateNumber : null,
+      token : "",
+      type : lic
+    }
+
+    db.collection("Drivers").doc(user).set(data)
+    closeAddModal()
     Swal.fire(
-        'Oopss!',
-        'Error 404!',
-        'info'
-      )
-  });  
+      'Muy bien!',
+      'Usuario creado!',
+      'success'
+    )
+    clearInputs()
+
+    // ...
+  })
+  .catch((error) => {
+
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+  }else{
+    Swal.fire(
+      'Oops!',
+      'Complete los campos!',
+      'info'
+    )
+  }
+
+}
+
+function getUsersFromDatabase(){
+
+  db.collection("Drivers").onSnapshot((querySnapshot) => {
+
+    let ctx = 0
+
+      users = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        
+        $('#tb-data').DataTable().destroy()
+        $("#tbody").html(
+      users
+        .map((v) => {
+
+          let t = v.type
+          let x = ""
+    
+          if(t == "1"){
+            x = "Administrador"
+          }else{
+            x = "Conductor"
+          }
+
+          ctx++
+
+          return `
+          <tr style="cursor: pointer">
+          <td><strong>${ctx}</strong></td>
+          <td>${v.lastname+" "+v.name}</td>
+          <td>${(v.dni)}</td>
+          <td>${v.email}</td>
+          <td>${v.phone}</td>
+          <td>${x}</td>
+          </tr>`;
+       
+         
+        })
+        .join("")
+    );
+
+    //console.log(students)
+    createDatatable()
+    localStorage.setItem("users",JSON.stringify(users))
+    document.getElementById("bgspinner").style = "display:none;"
+     
+           
+  }, (error) => {
+    console.log(error)
+}); 
+
+}
+
+function getUsersFromCache(){
+
+  let ctx = 0
+   
+    $('#tb-data').DataTable().destroy()
+    $("#tbody").html(
+  vCache
+    .map((v) => {
+
+      let t = v.type
+      let x = ""
+
+      if(t == "1"){
+        x = "Administrador"
+      }else{
+        x = "Conductor"
+      }
+
+      ctx++
+
+        return `
+        <tr style="cursor: pointer">
+        <td><strong>${ctx}</strong></td>
+        <td>${v.lastname+" "+v.name}</td>
+        <td>${(v.dni)}</td>
+        <td>${v.email}</td>
+        <td>${v.phone}</td>
+        <td>${x}</td>
+        </tr>`;
+     
+    })
+    .join("")
+);
+
+//console.log(students)
+createDatatable()
+document.getElementById("bgspinner").style = "display:none;"
+getUsersFromDatabase() 
 
 }
